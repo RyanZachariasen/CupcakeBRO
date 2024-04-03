9fae2a0697152ddc1efe036885bd824b1561a719
BekhanM <therealbekhan@gmail.com>
2024-04-03 10:14:28 +0200

    file structure update bekhan

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,36 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("eat ass");
+package app;
+
+import app.config.ThymeleafConfig;
+import app.controllers.UserController;
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+import app.controllers.TaskController;
+
+
+public class Main  {
+
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String DB = "fourthingsplus";
+
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+
+    public static void main(String[] args)
+    {
+        // Initializing Javalin and Jetty webserver
+
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/public");
+            config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
+        }).start(7070);
+
+        // Routing
+
+        app.get("/", ctx ->  ctx.render("index.html"));
+
+        UserController.addRoutes(app,connectionPool);
+        TaskController.addRoutes(app,connectionPool);
     }
 }
